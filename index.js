7df16c9d41fc059e5db734dd505858998d3be85f
Fulvio Salza <fsalza96@gmail.com>
2023-03-03 18:14:15 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age ++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event ) {
+    const messages = [];
+    for (let x = 0; x < names.length; x++) {
+        messages.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return messages;
+}
+
+writeCards(names, "surprise");
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
